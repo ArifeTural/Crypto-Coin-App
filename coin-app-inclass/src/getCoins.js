75782ae036c8f7b9833dc8f1e831b5b0a8b5bcd1
Q7135 <145381889+Q7135@users.noreply.github.com>
2024-08-17 02:51:18 +0300

    jkfdj

--- coin-app-inclass/src/getCoins.js
+++ coin-app-inclass/src/getCoins.js
@@ -1,3 +1,36 @@
-export const getCoins = (query) =>{
-console.log(query);
-}
+// export const getCoins = (query) =>{
+// console.log(query);
+// const url = `https://api.coinranking.com/v2/coins?search=${query}`;
+
+// const options = {
+//     headers: {
+//       'x-access-token': 'coinrankingfb7262aca66d8a48683b6657b1b550c3c8d00b52bff7fe43',
+//     },
+//   };
+  
+//   fetch(url, options)
+//     .then((response) => response.json())
+//     .then((result) => console.log(result));
+// }
+
+export const getCoins = async (query) =>{
+    console.log(query);
+    const url = `https://api.coinranking.com/v2/coins?search=${query}`;
+    
+    const options = {
+        headers: {
+          'x-access-token': 'coinrankingfb7262aca66d8a48683b6657b1b550c3c8d00b52bff7fe43',
+        },
+      };
+      try{
+        const res = await fetch(url,options)
+        const coinData = await res.json()
+        console.log(coinData);    
+      }catch(error){
+        console.log(error);
+
+      }
+       
+    }
+      
+   
