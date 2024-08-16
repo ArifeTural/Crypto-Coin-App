fe2a0e1096bbc383ddf0bafbf90903df6da2c518
Q7135 <145381889+Q7135@users.noreply.github.com>
2024-08-17 02:22:13 +0300

    getcoins

--- coin-app-inclass/main.js
+++ coin-app-inclass/main.js
@@ -1,5 +1,31 @@
 import "./scss/style.scss";
+import { getCoins } from "./src/getCoins.js";
 
-document.querySelector("#app").innerHTML= `
+const form = document.querySelector("header form");
+console.log(form);
 
-`
+
+form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+
+    getInputVal()
+    
+    form.reset()
+})
+
+const getInputVal = () =>{
+
+    const inputVal = document.querySelector("header form input").value
+    console.log(inputVal);
+
+    if(!inputVal.trim()){
+        alert("input not be blank!")
+    }else{
+        // console.log("input dolu");
+        getCoins(inputVal)
+    
+
+    }
+
+}
+
---
+++ coin-app-inclass/src/getCoins.js
@@ -0,0 +1,3 @@
+export const getCoins = (query) =>{
+console.log(query);
+}
