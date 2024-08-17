1579f6c255bd0f69f38729ef0361dd46eba543f6
Q7135 <145381889+Q7135@users.noreply.github.com>
2024-08-17 03:38:14 +0300

    işle

--- coin-app-inclass/src/getCoins.js
+++ coin-app-inclass/src/getCoins.js
@@ -1,66 +1,69 @@
-import axios from "axios" 
-import displayCoins from "./displayCoins.js";
+import axios from "axios"
+import displayCoins from "./displayCoins.js"
+//* named export
+// export const getCoins = (query) => {
+//   console.log(query);
+//   const url = `;https://api.coinranking.com/v2/coins?search=${query}`
 
-// export const getCoins = (query) =>{
-// console.log(query);
-// const url = `https://api.coinranking.com/v2/coins?search=${query}`;
-
-
-
-// const options = {
+//   const options = {
 //     headers: {
-//       'x-access-token': 'coinrankingfb7262aca66d8a48683b6657b1b550c3c8d00b52bff7fe43',
+//       "x-access-token":
+//         "coinranking3753c1847a9cdaadc193980cbd59edfcd1aa67efdb0fa6e9",
 //     },
+//     // method:"POST"
 //   };
-  
+
 //   fetch(url, options)
 //     .then((response) => response.json())
 //     .then((result) => console.log(result));
-// }
+// };
 
-// export const getCoins = async (query) =>{
-//     console.log(query);
-//     const url = `https://api.coinranking.com/v2/coins?search=${query}`;
-    
-//     const options = {
-//         headers: {
-//           'x-access-token': 'coinrankingfb7262aca66d8a48683b6657b1b550c3c8d00b52bff7fe43',
-//         },
-//       };
-//       try{
-//         const res = await fetch(url,options)
-//         const coinData = await res.json()
-//         console.log(coinData);    
-//       }catch(error){
-//         console.log(error);
+// export const getCoins = async (query) => {
+//   console.log(query);
+//   const url = `https://api.coinranking.com/v2/coins?search=${query}`;
 
-//       }
-       
-//     }
+//   const options = {
+//     headers: {
+//       "x-access-token":
+//         "coinranking3753c1847a9cdaadc193980cbd59edfcd1aa67efdb0fa6e9",
+//     },
+//     // method:"POST"
+//   };
+//   try {
+//     const res = await fetch(url, options);
+//     const coinData = await res.json();
+//     console.log(coinData);
+//   } catch (error) {
+//     console.error(error)
+//   }
+// };
+
+export const getCoins = async (query) => {
+  console.log(query);
+  const url = `https://api.coinranking.com/v2/coins?search=${query}`;
+
+  const options = {
+    headers: {
+      "x-access-token":
+        "coinranking3753c1847a9cdaadc193980cbd59edfcd1aa67efdb0fa6e9",
+    },
+    // method:"POST"
+  };
+  try {
+     const res = await axios(url,options) //* otomatik json
+    //  console.log(res)
+     console.log(res.data)
+     //* axios veriyi her zaman data keyinde verir. res den sonra yazdığımız data axiosa ait. ikinic data da api dean gelen veri data objesinde geldiği için
+     if(!res.data.data.coins.length){
+        alert("Coin not found!")
+     }else{
+        displayCoins(res.data.data.coins[0]);
+     }
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 
-    export const getCoins = async (query) =>{
-        console.log(query);
-        const url = `https://api.coinranking.com/v2/coins?search=${query}`;
-        
-        const options = {
-            headers: {
-              'x-access-token': 'coinrankingfb7262aca66d8a48683b6657b1b550c3c8d00b52bff7fe43',
-            },
-          };
-          try{
-           const res = await axios(url,options)
-           console.log(res.data);
-           if(!res.data.data.coins.lenght){
-            alert("Coin nnot found")
-           }else{
-            displayCoins(res.data.data.lenght[0])
-           }
-          }catch(error){
-            console.log(error);
-    
-          }
-           
-        }
       
    
